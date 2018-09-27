8d2ae983d58016cf2a7bb346bc38872f7a22c7ed
jc <spammailhere99@yahoo.com>
2018-09-26 19:56:24 -0500

    Open the websocket port 8546 and close the HTTP port 8545

--- geth.sh
+++ geth.sh
@@ -21,21 +21,24 @@ fi
 sudo docker run -d --rm \
     --name geth \
     -p 30303:30303 \
-    -p 8545:8545 \
+    -p 8546:8546 \
     -v $ETHCHAINPATH:/root/.ethereum \
     ethereum/client-go \
         --syncmode "full" \
         --ws \
         --wsaddr 0.0.0.0 \
         --wsport 8546 \
-        --wsorigins "*"
-        --rpccorsdomain "http://$(MYIP)" \
+        --wsorigins "*" \
         --ipcdisable
 
-# more flags
+# Flags to enable the websocked RPC server
+#--ws Enable the WS-RPC server
+#--wsaddr WS-RPC server listening interface (default: "localhost")
+#--wsport WS-RPC server listening port (default: 8546)
 #--wsapi API's offered over the WS-RPC interface (default: "eth,net,web3")
+#--wsorigins Origins from which to accept websockets requests
 
-# rpccorsdomain turns on "cross-origin resource sharing" to allow webpages
+#--rpccorsdomain turns on "cross-origin resource sharing" to allow webpages
 # served by the listed domain to access the rpc interface. If a simple status
 # page is served by the same machine, allow it access
 
